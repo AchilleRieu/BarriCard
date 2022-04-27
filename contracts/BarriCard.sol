b4ffee300ea13cfa97bab941f134f66b1543e551
Achille Rieu <achille.rieu@etu.emse.fr>
2022-04-27 09:10:11 +0100

    Adding comments and modifing function prototypes

--- contracts/BarriCard.sol
+++ contracts/BarriCard.sol
@@ -23,16 +23,39 @@ abstract contract BarriCard is ERC721, Ownable {
 
     mapping (uint8 => address) public cardToOwner;
     mapping (address => uint8) ownerCardCount;
+    mapping (address => uint8) ownerCardInDeckCount;
 
-    function selectCardsDeck() public {
-
+    function addCardInDeck() public { //passer une carte en parametre
+        //Check si ownerCardInDeckCount est plus petit que 10 (taille max du deck)
+        //Passer la variable isInDeck de la carte à False (revient a supprimer la carte du Deck)
+        //decrementer ownerCardInDeckCount de 1
     }
+
+    function removeCardInDeck() public { //passer une carte en parametre
+        //Check si ownerCardInDeckCount est plus grand que 0 (taille min du deck)
+        //Passer la variable isInDeck à True (revient a ajouter la carte au Deck)
+        //incrementer ownerCardInDeckCount de 1
+    }
+
+    //problème : pour afficher les cartes du Deck c'est compliqué (il faut parcourir toutes les cartes 
+    //  et selectionner seulement celles qui ont isInDeck a True
 
     function _cardShuffle() internal {
-
+        //Deux possibilites
+        //ajouter un variable à la structure de Card pour indiquer son rang dans le deck
+        //ne pas ecrire cette fonction et effectuer le choix aleatoire pdt cardBattle 
+        //  -> prendre une carte aléatoire parmis celle du joueur qui ont isInDeck à True et n'ont pas encore ete joué
+        //  Besoin d'ajouter une varaible : hasBeenPlayed dans dans la struct de card
     }
 
-    function _cardBattle() internal {
+    function _cardBattle() internal { //address des deux joueurs 
+        // Variable interne : compteur de point pour chaque joueur
+
+        //boucle x10 (ou max du nombre de carte dans les decks)
+            //comparer les cartes des deux joueurs et changer les variable internes en fonction
+        
+        //Retourner le resultat du match 
+        //  (voir pour modifier un variable globale, peut etre plus securisé)
 
     }
 }
