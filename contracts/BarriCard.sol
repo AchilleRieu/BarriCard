9924414cf67977cf89f35834c6ac397b1c01c035
Achille Rieu <achille.rieu@etu.emse.fr>
2022-05-04 22:46:50 +0100

    Coding cardBattle function

--- contracts/BarriCard.sol
+++ contracts/BarriCard.sol
@@ -24,6 +24,7 @@ abstract contract BarriCard is ERC721, Ownable {
     mapping (uint => address) public cardToOwner;
     mapping (address => uint8) ownerCardCount;
     mapping (address => uint8) ownerCardInDeckCount;
+    mapping (address => uint8) OwnerToWin;
 
     modifier onlyOwnerOf(uint _cardId) {
         require(msg.sender == cardToOwner[_cardId]);
@@ -51,15 +52,26 @@ abstract contract BarriCard is ERC721, Ownable {
     //problème : pour afficher les cartes du Deck c'est compliqué (il faut parcourir toutes les cartes 
     //  et selectionner seulement celles qui ont isInDeck a True
 
-    function _cardShuffle() internal {
+    function _cardShuffle(uint[] memory deck) internal view {
         //Deux possibilites
         //ajouter un variable à la structure de Card pour indiquer son rang dans le deck
         //ne pas ecrire cette fonction et effectuer le choix aleatoire pdt cardBattle 
         //  -> prendre une carte aléatoire parmis celle du joueur qui ont isInDeck à True et n'ont pas encore ete joué
         //  Besoin d'ajouter une varaible : hasBeenPlayed dans dans la struct de card
+
+        for (uint256 i = 0; i < deck.length; i++) {
+            uint256 n = i + uint256(keccak256(abi.encodePacked(block.timestamp))) % (deck.length - i);
+            uint256 temp = deck[n];
+            deck[n] = deck[i];
+            deck[i] = temp;
+        }
     }
 
-    function _cardBattle() internal { //address des deux joueurs 
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a <= b ? a : b;
+    }
+
+    function _cardBattle(address adrj1, address adrj2) external { //address des deux joueurs 
         // Variable interne : compteur de point pour chaque joueur
 
         //boucle x10 (ou max du nombre de carte dans les decks)
@@ -68,6 +80,28 @@ abstract contract BarriCard is ERC721, Ownable {
         //Retourner le resultat du match 
         //  (voir pour modifier un variable globale, peut etre plus securisé)
 
+        uint[] memory deck1 = getCardsInDeckByOwner(adrj1);
+        _cardShuffle(deck1);
+        uint[] memory deck2 = getCardsInDeckByOwner(adrj2);
+        _cardShuffle(deck2);
+
+        int8 score = 0;
+
+        for (uint i = 0; i < min(deck1.length, deck2.length); i++){
+            if (deck1[i]>deck2[i]) {
+                score++;
+            }
+            else if (deck1[i]<deck2[i]){
+                score--;
+            }
+        }
+
+        if (score>0){
+            OwnerToWin[adrj1]++;
+        }
+        else if (score<0) {
+            OwnerToWin[adrj2]++;
+        }
     }
 
     function getCardsByOwner(address _owner) external view returns(uint[] memory) {
@@ -82,7 +116,7 @@ abstract contract BarriCard is ERC721, Ownable {
         return result;
     }
 
-    function getCardsInDeckByOwner(address _owner) external view returns(uint[] memory) {
+    function getCardsInDeckByOwner(address _owner) internal view returns(uint[] memory) {
         uint[] memory result = new uint[](ownerCardInDeckCount[_owner]);
         uint counter = 0;
         for (uint i = 0; i < cards.length; i++) {
